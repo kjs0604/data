dfd6278f8993814c81074c8dea4acc5d9911bdb9
kjs0604 <kjs3886@jbanu.ac.kr>
2023-03-27 11:16:19 +0900

    second file

---
+++ Project3 kjs/소스.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int findArryaMax(int score[], int n)
+{
+	int tmp = score[0];
+	for (int i = 1; i < n; i++) {
+		if (score[i] > tmp) {
+			tmp = score[i];
+
+		}
+	}
+	return tmp;
+}
+void main()
+{
+	int score[5] = { 12, 123, 12, 54, 1,};
+	int out;
+	out = findArryaMax(score, 5);
+	printf("result = %d", out);
+}
